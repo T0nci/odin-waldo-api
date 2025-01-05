29f865c6982e47b1b74df9848c4882e28ad5374d
T0nci <138928016+T0nci@users.noreply.github.com>
2025-01-05 15:44:05 +0100

    Add express packages, setup cors and custom errors

---
+++ app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const CustomError = require("./utils/CustomError");
+
+const app = express();
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+  }),
+);
+
+app.use(express.json());
+
+app.use((req, res, next) => next(new CustomError("Not Found", 404)));
+// eslint-disable-next-line no-unused-vars
+app.use((error, req, res, next) => {
+  if (!error.statusCode) {
+    console.error(error);
+    error.statusCode = 500;
+    error.message = "Internal Server Error";
+  }
+
+  res
+    .status(error.statusCode)
+    .json({ error: `${error.statusCode}: ${error.message}` });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Waldo API listening on port ${PORT}!`));
