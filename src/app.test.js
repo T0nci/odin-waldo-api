b4d31013f819b4084792e62b0864bb632267af39
T0nci <138928016+T0nci@users.noreply.github.com>
2025-01-10 18:21:15 +0100

    Write tests for the route that starts the game

--- src/app.test.js
+++ src/app.test.js
@@ -5,6 +5,7 @@ const {
   expect,
   beforeAll,
   afterAll,
+  afterEach,
 } = require("@jest/globals");
 const prisma = require("./client");
 const app = require("./app");
@@ -58,5 +59,51 @@ describe("indexRouter", () => {
       "application/json; charset=utf-8",
     );
     expect(response.body).toStrictEqual(mapsWithoutId);
+  });
+});
+
+describe("gameRouter", () => {
+  beforeAll(async () => {
+    await prisma.map.createMany({
+      data: maps,
+    });
+  });
+
+  afterAll(async () => {
+    await prisma.map.deleteMany();
+  });
+
+  afterEach(async () => {
+    await prisma.user.deleteMany();
+  });
+
+  test("/start/:mapId returns error when mapId is invalid", async () => {
+    const response = await request(app)
+      .get("/game/start/invalidMapId")
+      .set("Accept", "application/json; charset=utf-8");
+
+    expect(response.status).toBe(400);
+    expect(response.header["content-type"]).toBe(
+      "application/json; charset=utf-8",
+    );
+    expect(response.body).toStrictEqual({
+      error: "Invalid map ID.",
+    });
+  });
+
+  test("/start/:mapId returns token and creates user successfully", async () => {
+    const response = await request(app)
+      .get("/game/start/1")
+      .set("Accept", "application/json; charset=utf-8");
+
+    const user = await prisma.user.findFirst();
+
+    expect(response.status).toBe(200);
+    expect(response.header["content-type"]).toBe(
+      "application/json; charset=utf-8",
+    );
+    expect(typeof response.body.token).toBe("string");
+    expect(typeof user).toBe("object");
+    expect(user.map_id).toBe(1);
   });
 });
